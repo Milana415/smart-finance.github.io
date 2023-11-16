c832fd6475652b318b4a4972c5165f4c7101b07b
Milana Muslivchik <137694394+Milana415@users.noreply.github.com>
2023-11-16 23:19:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function check() {
+    let trueAnswer = 0, falseAnswer = 0
+    let testResult="";
+    let choise;
+    for (let l=1; l<=5;l++) {
+        let quetion = document.forms["test"].elements["question"+l];
+        for (let i=1; l<=quetion.lenght; i++){
+            if (l[i].checked) {
+                choice==l[i].value
+            }
+            if (l[i].checked) {
+                choice=l[i].value
+            }
+        }
+    }
+    alert()
+}
+    //test-radiobutton
+$(document).ready(function () {
+    $.each($('.'))
+})
